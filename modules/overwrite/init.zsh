016efec03c9400611ab84e053b92628e2e1dad69
Tobias Wohlfarth <tobias.wohlfarth@googlemail.com>
2017-07-20 15:51:50 +0200

    add: change some keybings via overwrite module

--- modules/overwrite/init.zsh
+++ modules/overwrite/init.zsh
@@ -4,3 +4,19 @@
 # Authors:
 #   Tobias Wohlfarth <tobias.wohlfarth@googlemail.com>
 #
+
+# Editor: Inserts '_ ' (alias for sudo) at the beginning of the line.
+sudoalias=$(alias | grep "=sudo" | head -n 1 | sed 's/=sudo//g')
+function prepend-sudo {
+  if [[ "$BUFFER" != su(do|)\ * ]]; then
+    BUFFER="$sudoalias $BUFFER"
+    (( CURSOR += 2 ))
+  fi
+}
+zle -N prepend-sudo
+
+# Undo.
+bindkey "^Z" undo
+
+# Redo.
+bindkey "^Y" redo
